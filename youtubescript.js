502bcfddc9132b6231bc3161804c90f61820a94e
Cal-culator <calvinchen99@gmail.com>
2021-04-14 11:33:37 -0700

    updated for games

--- youtubescript.js
+++ youtubescript.js
@@ -31,14 +31,31 @@ var moveFile = async (file, dir2)=>{
     return;
 };
 
-
+var username;
+var password;
+var tags;
 
 
 var game = process.argv[2];
-const username = config[game].username;
-const password = config[game].password;
-const tags = config[game].tags;
-
+if (game == "pubg_mobile")
+{
+    
+    username = config["PUBGM"].username;
+    password = config["PUBGM"].password;
+    tags = config["PUBGM"].tags;
+}
+else if (game =="free_fire")
+{
+    username = config["Free Fire"].username;
+    password = config["Free Fire"].password;
+    tags = config["Free Fire"].tags;
+}
+else
+{
+    username = config[game].username;
+    password = config[game].password;
+    tags = config[game].tags;
+}
 
 var upload = async (page, filePath, creatorName, videoTitle) =>
 {
@@ -59,28 +76,33 @@ var upload = async (page, filePath, creatorName, videoTitle) =>
     description = inputBoxes[1];
     
     //rename title
-    page.waitFor(1000);
+    page.waitFor(2000);
     //const input = await page.$('ytcp-mention-input[class="style-scope ytcp-mention-textbox"]');
     
     await title.click({ clickCount: 3 });
-    await title.type(videoTitle);
+    await title.type(videoTitle.substring(0,videoTitle.length-4));
     
     await page.waitFor(500);
     
     //const input = await page.$('ytcp-mention-input[class="style-scope ytcp-mention-textbox"]'[1]);
     await description.click({ clickCount: 3 });
-    await description.type(`Hello everyone! Welcome to the ggTV's official youtube channel. I do not take credit for any of the clips.\n\nOur app is coming soon! Visit www.ggtv.co for more information!\n\nEmail: ggtvdotin@gmail.com\nIG: https://www.instagram.com/ggtv_app/\nDiscord: https://discord.gg/T9AVnNJ3NW\n\nVideo Credit: ${creatorName}\n\nThanks for watching and consider subscribing for more such videos.`);
+    await description.type(`Join ggTV Army 5000 INR Challenges at https://discord.gg/NpD3z4h7Kt\nDownload our app at https://play.google.com/store/apps/details?id=com.ggtv\nI do not take credit for any of the clips.\nVisit www.ggtv.co for more information!\n\nEmail: ggtvdotin@gmail.com\nIG: https://www.instagram.com/ggtv_army/\n\nVideo Credit: ${creatorName}\n\nThanks for watching and consider subscribing for more such videos.`);
     
     //click not made for kids
-    await page.waitFor(1000);
-    await page.click('paper-radio-button[name="NOT_MADE_FOR_KIDS"]');
+    console.log("u-1");
+    await page.waitForSelector('tp-yt-paper-radio-button[name="NOT_MADE_FOR_KIDS"]');
+    await page.click('tp-yt-paper-radio-button[name="NOT_MADE_FOR_KIDS"]');
     
     //click More Options
-    await page.waitFor(1000);
-    await page.click('ytcp-button[label="More options"]');
+    //await page.waitFor(2000);
+    console.log("u-2");
+    await page.waitFor('ytcp-button[id="toggle-button"]');
+    await page.click('ytcp-button[id="toggle-button"]');
 
     //add Tags
-    await page.waitFor(1000);
+    //await page.waitFor(1000);
+    console.log("u-3");
+    await page.waitForSelector('input[placeholder="Add tag"]');
     await page.type('input[placeholder="Add tag"]', tags);
 
     //select Language
@@ -120,18 +142,20 @@ var upload = async (page, filePath, creatorName, videoTitle) =>
             await gameCategories[i].click();
         }
     }
-    
-    await page.type('input[aria-label="Game title (optional)"]', game);
-    await page.waitFor(2000);
-    
+    if (game=="pubg_mobile")
+    {
+        await page.type('input[aria-label="Game title (optional)"]', "PUBG Mobile");
+        await page.waitFor(2000);
+    }
+    else
+    {
+        await page.type('input[aria-label="Game title (optional)"]', game);
+        await page.waitFor(2000);
+    }
     await page.keyboard.press('ArrowDown');
     await page.waitFor(100);
     await page.keyboard.press('ArrowDown');
-    if (game == "PUBGM")
-    {
-        await page.waitFor(100);
-        await page.keyboard.press('ArrowDown');
-    }
+
     
     await page.keyboard.press(String.fromCharCode(13));
 
@@ -143,9 +167,11 @@ var upload = async (page, filePath, creatorName, videoTitle) =>
     //need to add cards later
     await page.waitFor(1000);
     await page.click('ytcp-button[id="next-button"]');
-    
+    await page.waitFor(1000);
+    await page.click('ytcp-button[id="next-button"]');
+    await page.waitFor(1000);
     //click Public    await page.waitFor(500);
-    await page.click('paper-radio-button[name="PUBLIC"]');
+    await page.click('tp-yt-paper-radio-button[name="PUBLIC"]');
     
     //click publish
     await page.click('ytcp-button[id="done-button"]');
@@ -179,19 +205,15 @@ var upload = async (page, filePath, creatorName, videoTitle) =>
             headless: false,
         });
 
-
-
-    
-    
-    
     const page = await browser.newPage();
     navigationPromise = page.waitForNavigation();
     await page.goto("https://youtube.com");
     await navigationPromise;
     //press sign in
-    await page.waitForSelector('paper-button[aria-label="Sign in"]');
-    await page.click('paper-button[aria-label="Sign in"]');
-    await page.waitFor(1000);
+    await page.waitForSelector('tp-yt-paper-button[aria-label="Sign in"]');
+    await page.click('tp-yt-paper-button[aria-label="Sign in"]');
+    //await page.waitFor(1000);
+    console.log(1);
     
     //type in login credentials
     await page.waitForSelector('input[type="email"]');
@@ -199,7 +221,8 @@ var upload = async (page, filePath, creatorName, videoTitle) =>
     await page.type('input[type="email"]', username);
     await page.keyboard.press(String.fromCharCode(13));
     
-    await page.waitFor(2000);
+    //await page.waitFor(2000);
+    console.log(2);
     
     await page.waitForSelector('input[type="password"]');
     await page.click('input[type="password"]');
@@ -207,10 +230,8 @@ var upload = async (page, filePath, creatorName, videoTitle) =>
     await page.type('input[type="password"]', password);
     await page.keyboard.press(String.fromCharCode(13));
     
-    await page.waitFor(10000);
-    
-
-    
+    //await page.waitFor(10000);
+    console.log(3);
 
     if (game == "Minecraft")
     {
@@ -234,7 +255,7 @@ var upload = async (page, filePath, creatorName, videoTitle) =>
 
         await page.waitFor(8000);
     }
-    if (game == "PUBGM")
+    if (game == "pubg_mobile")
     {
         //await page.waitForSelector('input[value="112345903986598661235"]');
         //await page.click('input[value="112345903986598661235"]');
@@ -278,13 +299,35 @@ var upload = async (page, filePath, creatorName, videoTitle) =>
 
         await page.waitFor(8000);
     }
+    if (game == "free_fire")
+    {
+        //await page.waitForSelector('input[value="112345903986598661235"]');
+        //await page.click('input[value="112345903986598661235"]');
+        await page.waitForSelector('yt-formatted-string[id="channel-title"]');
+        var channels = await page.$$('yt-formatted-string[id="channel-title"]');
+        
+        //console.log("HERE");
+        for (i in channels)
+        {
+            var channelName = await (await(channels[i].getProperty('textContent'))).jsonValue();
+            //console.log(channelName);
+            if (channelName == 'ggTV: Garena Free Fire')
+            {
+                await page.waitFor(1000);
+                await channels[i].click();
+                break;
+            }
+        }
 
+        await page.waitFor(8000);
+    }
+    
     
     await page.waitForSelector('img[alt="Avatar image"]');
     await page.click('img[alt="Avatar image"]');
     
-    await page.waitFor(1000);
-    
+    //await page.waitFor(1000);
+    console.log(4);
     
     await page.waitForSelector('a[href="https://studio.youtube.com/"]');
     await page.click('a[href="https://studio.youtube.com/"]');
@@ -294,7 +337,7 @@ var upload = async (page, filePath, creatorName, videoTitle) =>
     await page.setBypassCSP(true)
     
     //Where I cut
-    for (let i=0; i < 60; i++)
+    for (let i=0; i < 400; i++)
     {
         let file = editJsonFile('./config.json');
         console.log(i);
